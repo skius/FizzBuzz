17ab4d586c4c410c03e6a7516e3b524393acd912
Niels Saurer <me@nielssaurer.com>
2019-07-24 16:30:43 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	const max = 1000
+
+	m := funcTimer(masterString, max)
+	d := funcTimer(dynamicMasterString, max)
+	h := funcTimer(hardcoded, max)
+
+	fmt.Println()
+	fmt.Println(m)
+	fmt.Println(d)
+	fmt.Println(h)
+}
+
+func funcTimer(f func(int), max int) int64 {
+	start := time.Now().UnixNano()
+	f(max)
+	end := time.Now().UnixNano()
+	return totalTime(start, end)
+}
+
+func hardcoded(max int) {
+	for i := 1; i <= max; i++ {
+		if i % 15 == 0 {
+			fmt.Print("FizzBuzz ")
+		} else if i % 5 == 0 {
+			fmt.Print("Buzz ")
+		} else if i % 3 == 0 {
+			fmt.Print("Fizz ")
+		}
+		fmt.Println(i)
+	}
+}
+
+func masterString(max int) {
+	for i := 1; i <= max; i++ {
+		str := ""
+		if i % 3 == 0 {
+			str += "Fizz"
+		}
+		if i % 5 == 0 {
+			str += "Buzz"
+		}
+		fmt.Print(str + " ")
+		fmt.Println(i)
+	}
+}
+
+func dynamicMasterString(max int) {
+	divisibilityChecker := func(i, divisor int, output string) string {
+		if i % divisor == 0 {
+			return output
+		}
+		return ""
+	}
+
+	fizzMap := map[int]string{
+		3: "Fizz",
+		5: "Buzz",
+	}
+
+	for i := 1; i <= max; i++ {
+		str := ""
+		for divisor, word := range fizzMap {
+			str += divisibilityChecker(i, divisor, word)
+		}
+
+		if str == "" {
+			fmt.Println(i)
+		} else {
+			fmt.Println(str)
+		}
+
+	}
+}
+
+func totalTime(start int64, end int64) int64 {
+	return end - start
+}
